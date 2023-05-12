44fa8e8730e7cce283881a2eaddd3d9ee13f4801
Michal Marciniak <gp3510@gmail.com>
2023-05-13 00:11:28 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hi There!"));
+app.get("/", (req, res) =>
+  res.send(
+    "Hi There! New deployment, approved with manual confirmation in the DevOps pipeline."
+  )
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
